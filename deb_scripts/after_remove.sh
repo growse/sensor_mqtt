0e5d00c03f64ffc156194eecd72bb475b1435ae9
Andrew Rowson <github@growse.com>
2020-03-27 15:37:38 +0000

    More resilient deb_scrpts, added before_upgrade

--- deb_scripts/after_remove.sh
+++ deb_scripts/after_remove.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-userdel sensor_mqtt
+userdel sensor_mqtt || echo "Unable delete user 'sensor_mqtt'"
--- deb_scripts/before_install.sh
+++ deb_scripts/before_install.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-useradd -s /bin/false -M -G i2c sensor_mqtt
+useradd -s /bin/false -M -G i2c sensor_mqtt || echo "User already exists"
+usermod -g i2c sensor_mqtt
