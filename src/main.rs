f5ca87178da33d57c53bfcae57b2d9b3290aff2b
Andrew Rowson <github@growse.com>
2020-09-06 14:27:30 +0100

    Publishing needs a loop

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,38 @@ extern crate confy;
 #[macro_use]
 extern crate log;
 
-use bme280::{Measurements, BME280};
-use confy::ConfyError;
 use core::fmt;
+use std::error::Error;
+
+use bme280::{Measurements, BME280};
 use env_logger::Env;
 use i2cdev::linux::LinuxI2CError;
 use linux_embedded_hal::{Delay, I2cdev};
+use rumqttc::Incoming::PubAck;
+use rumqttc::{Client, Incoming, MqttOptions, Outgoing, QoS};
 use serde::export::Formatter;
 use serde_derive::{Deserialize, Serialize};
-use std::error::Error;
+use std::process::exit;
 
-fn main() {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
-    let result = load_config()
-        .and_then(|config| -> Result<Measurements<_>, Box<dyn Error>> {
-            read_bme280(config.i2c_bus_path)
-        })
-        .and_then(send_to_mqtt);
-
-    match result {
-        Ok(_) => info!("GREAT SUCCESS"),
-        Err(e) => error!("{}", e),
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 struct Configuration {
     i2c_bus_path: String,
+    mqtt_host: String,
+    mqtt_port: u16,
+    mqtt_username: String,
+    mqtt_password: String,
+    mqtt_topic_base: String,
 }
 
 impl Default for Configuration {
     fn default() -> Self {
         Self {
-            i2c_bus_path: "/dev/i2c-1".into(),
+            i2c_bus_path: "/dev/i2c-1".to_string(),
+            mqtt_host: "localhost".to_string(),
+            mqtt_port: 1883,
+            mqtt_username: "".to_string(),
+            mqtt_password: "".to_string(),
+            mqtt_topic_base: "sensors".to_string(),
         }
     }
 }
@@ -44,17 +43,38 @@ The bme280 crate doesn't implement Error for their errors, so we have to wrap
  */
 #[derive(Debug)]
 struct BME280ErrorWrapper(bme280::Error<LinuxI2CError>);
+
 impl fmt::Display for BME280ErrorWrapper {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self.0 {
+        match &self.0 {
             bme280::Error::InvalidData => write!(f, "Invalid Data"),
-            _ => write!(f, "Erm"),
+            bme280::Error::CompensationFailed => write!(f, "Compensation Failed"),
+            bme280::Error::NoCalibrationData => write!(f, "No Calibration Data"),
+            bme280::Error::UnsupportedChip => write!(f, "Unsupported Chip"),
+            bme280::Error::I2c(e) => write!(f, "I2c error {}", e),
         }
     }
 }
+
 impl Error for BME280ErrorWrapper {}
 
-fn read_bme280(i2c_bus_path: String) -> Result<Measurements<LinuxI2CError>, Box<dyn Error>> {
+fn main() {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    let config = load_config().unwrap_or_else(|e| {
+        error!("Unable to load config: {}", e);
+        exit(2)
+    });
+
+    let result = read_bme280(&config.i2c_bus_path)
+        .and_then(|measurements| send_to_mqtt(measurements, &config));
+
+    match result {
+        Ok(_) => info!("GREAT SUCCESS"),
+        Err(e) => error!("{}", e),
+    }
+}
+
+fn read_bme280(i2c_bus_path: &str) -> Result<Measurements<LinuxI2CError>, Box<dyn Error>> {
     debug!("Reading i2c bus at {}", i2c_bus_path.clone());
 
     let i2c_bus =
@@ -65,8 +85,50 @@ fn read_bme280(i2c_bus_path: String) -> Result<Measurements<LinuxI2CError>, Box<
     Ok(m)
 }
 
-fn send_to_mqtt(measurements: Measurements<LinuxI2CError>) -> Result<(), Box<dyn Error>> {
-    Err("Not implemented".into())
+fn send_to_mqtt(
+    _measurements: Measurements<LinuxI2CError>,
+    config: &Configuration,
+) -> Result<(), Box<dyn Error>> {
+    let mut mqtt_options = MqttOptions::new(
+        "sensor-mqtt-client",
+        config.mqtt_host.as_str(),
+        config.mqtt_port,
+    );
+
+    mqtt_options.set_credentials(config.mqtt_username.as_str(), config.mqtt_password.as_str());
+
+    let (mut client, mut connection) = Client::new(mqtt_options, 1);
+
+    let publish = client.publish(
+        format!(
+            "{topic_base}/{hostname}/{sensor_name}",
+            topic_base = config.mqtt_topic_base.as_str(),
+            hostname = "toot",
+            sensor_name = "parp"
+        ),
+        QoS::AtLeastOnce,
+        false,
+        "Hi".as_bytes(),
+    );
+
+    if publish.is_err() {
+        return Err(publish.err().unwrap().into());
+    };
+    for (_i, notification) in connection.iter().enumerate() {
+        match notification {
+            Ok(success_notification) => match success_notification {
+                (None, Some(Outgoing::Publish(p))) => println!("Publishing MQTT... id={:?}", p),
+                (Some(Incoming::Connected), None) => println!("MQTT Connected"),
+                (Some(Incoming::PubAck(pub_ack)), None) => {
+                    println!("MQTT published id={:?}", pub_ack.pkid)
+                }
+                _ => println!("Shrug"),
+            },
+            Err(e) => return Err(e.into()),
+        }
+    }
+
+    Ok(())
 }
 
 fn load_config() -> Result<Configuration, Box<dyn Error>> {
