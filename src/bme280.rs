bf75fe3d41406e98c9ae0cc6420620419a420a2b
Andrew Rowson <github@growse.com>
2022-04-12 20:03:38 +0100

    Upgrade BME280 to 0.4.1

--- src/bme280.rs
+++ src/bme280.rs
@@ -3,17 +3,17 @@ The bme280 crate doesn't implement Error for their errors, so we have to wrap
  */
 use crate::configuration::Configuration;
 use crate::MessageToPublish;
-use bme280::{Measurements, BME280};
 use core::fmt;
 
 use anyhow::Result;
-use linux_embedded_hal::i2cdev::linux::LinuxI2CError;
-use linux_embedded_hal::{Delay, I2cdev};
+use bme280::i2c::BME280;
+use bme280::Measurements;
+use linux_embedded_hal::{Delay, I2CError, I2cdev};
 use std::error::Error;
 use std::fmt::Formatter;
 
 #[derive(Debug)]
-pub struct BME280ErrorWrapper(pub bme280::Error<LinuxI2CError>);
+pub struct BME280ErrorWrapper(pub bme280::Error<I2CError>);
 
 impl fmt::Display for BME280ErrorWrapper {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -22,25 +22,25 @@ impl fmt::Display for BME280ErrorWrapper {
             bme280::Error::CompensationFailed => write!(f, "Compensation Failed"),
             bme280::Error::NoCalibrationData => write!(f, "No Calibration Data"),
             bme280::Error::UnsupportedChip => write!(f, "Unsupported Chip"),
-            bme280::Error::I2c(e) => write!(f, "I2c error {}", e),
+            _ => write!(f, "Unknown error"),
         }
     }
 }
 
 impl Error for BME280ErrorWrapper {}
 
-pub fn read_bme280(i2c_bus_path: &str) -> Result<Measurements<LinuxI2CError>> {
+pub fn read_bme280(i2c_bus_path: &str) -> Result<Measurements<I2CError>> {
     debug!("Reading i2c bus at {i2c_bus_path}");
-    let i2c_bus =
-        I2cdev::new(i2c_bus_path).map_err(|e| BME280ErrorWrapper(bme280::Error::I2c(e)))?;
-    let mut bme280 = BME280::new_primary(i2c_bus, Delay);
-    bme280.init().map_err(BME280ErrorWrapper)?;
-    let m = bme280.measure().map_err(BME280ErrorWrapper)?;
+    let i2c_bus = I2cdev::new(i2c_bus_path)?;
+    let mut bme280 = BME280::new_primary(i2c_bus);
+    let mut delay = Delay {};
+    bme280.init(&mut delay).map_err(BME280ErrorWrapper)?;
+    let m = bme280.measure(&mut delay).map_err(BME280ErrorWrapper)?;
     Ok(m)
 }
 
 pub fn measurements_to_messages(
-    measurements: Measurements<LinuxI2CError>,
+    measurements: Measurements<I2CError>,
     config: &Configuration,
 ) -> Result<Vec<MessageToPublish>> {
     let topic = format!(
